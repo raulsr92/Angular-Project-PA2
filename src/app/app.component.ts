716441d165ae8b67a87e8772694554ac0a4551ef
Raul Sanchez R. <raul.sr92@gmail.com>
2025-05-23 13:55:06 -0500

    Creacion y ubicacion de componentes + carga de imágenes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CarruselComponent } from './components/carrusel/carrusel.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CarruselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
