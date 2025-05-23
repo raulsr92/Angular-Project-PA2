6547ed19f72d7659e036bedccf94718a366450c9
Raul Sanchez R. <raul.sr92@gmail.com>
2025-05-23 15:15:56 -0500

    Avance de la cabecera pantallas grandes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
+  public headerOptions:string[] = ['Jerseys','Trainignwear','Fashion','Accessories','Gifts'];
+
+  public  headerIcons:string[] = [`<i class="bi bi-search"></i>`,
+                                  `<i class="bi bi-person"></i>`,
+                                  `<i class="bi bi-suit-heart"></i>`,
+                                  `<i class="bi bi-bag"></i>`]
 }
